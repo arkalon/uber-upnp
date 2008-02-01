300374b9439b785355ea57d1cbd0aa1b3c4c5a34
kenth.fagerlund <kenth.fagerlund@c7f95573-1745-0410-88bb-03385e41c32b>
2008-02-01 16:48:01 +0000

    Weekend Commit

---
+++ trunk/Uber-UPnP/src/eu/kenth/upnp/devicearch/Device.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.kenth.upnp.devicearch;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author developer(Admin)
+ */
+public class Device {
+
+    private String specVersion;
+    private String deviceType;
+    private String friendlyName;
+    private String URLBase;
+    
+    private String manufacturerName;
+    private String manufacturerURL;
+    
+    private String modelDescription;
+    private String modelName;
+    private String modelNumber;
+    private String modelURL;
+    private String serialNumber;
+    
+    private String UDN;
+    private String UPC;
+    
+    private ArrayList<Icon> iconList;
+    private ArrayList<Service> serviceList;
+    private ArrayList<Device> deviceList;
+    
+    private String presentationURL;
+    
+    
+    
+    
+    
+    
+}
